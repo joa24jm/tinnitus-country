0d3a11bcb0a4ab87bebf2633ad8ed1b629779b81
Johannes A <49393016+Luftnummer@users.noreply.github.com>
2021-02-26 15:28:18 +0100

    read in and convert tables from phpmyadmin to one dataframe

---
+++ src/d00_utils/utilities.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 09:41:01 2021
+
+@author: joa24jm
+
+Helper function for this project
+
+"""
+
+def unroll_baseline(ans):
+    """
+    Convert answer dataframe with one column for all answers of all questions
+    into a dataframe that has one column per question containing the answers
+    Additionally, safe the date when this user filled out the questionnaire
+
+    Parameters
+    ----------
+    ans : dataframe with answers from baseline questionnaire
+
+    Returns
+    -------
+    unrolled answers with user_id as index and one column per question
+
+    """
+    import pandas as pd
+
+    list_of_question_ids = ans['question_id'].dropna().unique()
+    list_of_users = [int(u) for u in ans['user_id'].dropna().unique()]
+    
+    
+    # result dataframe with user_id as index
+    answers_per_user = pd.DataFrame(data = None, 
+                                    index = list_of_users)
+    
+    # turn one column for all questions into one column per question
+    for q_id in list_of_question_ids:
+        # get all answers of all users for this question id
+        res = ans[ans.question_id == q_id][['user_id', 'answer']]
+        # drop users that has na values for their user_id
+        res = res[res['user_id'].notna()]
+        # convert user_ids to int32
+        res = res.astype({'user_id':'int32'})
+        # drop duplicates and keep the first user_id
+        res.drop_duplicates(subset=['user_id'], inplace = True)
+        # use the user_id as an index
+        res.set_index('user_id', drop = True, inplace = True)
+        # rename column to q_id
+        res.rename(columns = {'answer': f'{int(q_id)}'},inplace = True)
+        # concat res to answer_per_user on user_id
+        answers_per_user = pd.concat([answers_per_user, res], axis = 1)
+
+    # safe the date for each user
+    date = ans.drop_duplicates(subset = ['user_id'])[['user_id', 'created_at']]
+    # drop users that has na values for their user_id
+    date = date[date['user_id'].notna()]
+    # use the user_id as an index
+    date.set_index('user_id', drop = True, inplace = True)
+    # concat with answers_per_user
+    answers_per_user = pd.concat([answers_per_user, date], axis = 1)
+    
+    return answers_per_user
+
+def remove_testusers(df, column = 'user_id'):
+    """
+    Removes all rows in dataframe that contains a testuser
+
+    Parameters
+    ----------
+    df : dataframe with user_id as index or column
+    column : Name of user_id column. The default is 'user_id'.
+
+    Returns
+    -------
+    Cleaned dataframe without test_users
+
+    """
+
+    testusers = [553, 2186, 2244, 2242, 1563, 1119, 845, 374, 115, 54, 
+                 1, 2, 11, 49, 36, 48, 461, 41, 39, 553, 728, 43, 47, 64, 66]
+    
+    # if user_id is a column
+    try: 
+        df = df[~df[f'{column}'].isin(testusers)]
+    
+    except:
+        
+        # if user_id is an index
+        try: 
+            bad_df = df.index.isin(testusers)
+            df = df[~bad_df]
+        
+        # this should never raise   
+        except: 
+            raise ValueError('user_id is no column and no index.')
+    
+    return df
--- src/d01_data/csv-to-df.py
+++ src/d01_data/csv-to-df.py
@@ -4,32 +4,74 @@ Created on Thu Feb 25 14:56:24 2021
 
 @author: joa24jm
 
-This file reads in the JSON files from data/02_intermediate
+This file reads in the csv files from data/01_raw
 and converts them into a pandas dataframe.
-The json files in 01_raw contain a list [] with meta data in the beginning.
-These lists have been removed and the files have been saved in 02_intermediate.
+The files have been exported from phpmyAdmin using the 'csv for ms excel' option.
 """
 
 proj_loc = 'C:/Users/joa24jm/Documents/tinnitus-country/'
 
 #%% imports
 import pandas as pd
+import utilities as utils
 
 #%% read in dataframes
-ans =   pd.read_csv(proj_loc + 'data/01_raw/answers.csv', delimiter = ';')
+baseline =   pd.read_csv(proj_loc + 'data/01_raw/answers.csv', delimiter = ';')
 qns =   pd.read_csv(proj_loc + 'data/01_raw/questions.csv', delimiter = ';')
-sdans = pd.read_csv(proj_loc + 'data/01_raw/standardanswers.csv', delimiter = ';')
+daily = pd.read_csv(proj_loc + 'data/01_raw/standardanswers.csv', delimiter = ';')
 users = pd.read_csv(proj_loc + 'data/01_raw/users.csv', delimiter = ';')
 users_meta = pd.read_csv(proj_loc + 'data/01_raw/users_metadata.csv', delimiter = ';')
+survey = pd.read_csv(proj_loc + 'data/01_raw/umfrageonline-1277718.csv', delimiter = ';')
 
 #%% parse answers df and save every question into a new column
-list_of_question_ids = ans['question_id'].dropna().unique()
-list_of_users = ans['user_id'].dropna().unique()
+baseline = utils.unroll_baseline(baseline)
 
-answers_per_user = pd.DataFrame(data = None, 
-                                index = list_of_users, 
-                                columns = list_of_question_ids)
+# remove testusers
+baseline = utils.remove_testusers(baseline)
+daily = utils.remove_testusers(daily)
+users = utils.remove_testusers(users, column = 'id')
+users_meta = utils.remove_testusers(users_meta)
 
-for q_id in list_of_question_ids:
-    res = ans[ans.question_id == q_id][['user_id', 'answer']]
-    s = pd.Series()
+# merge baseline questionnaire with daily
+merged_df = pd.merge(daily, baseline, left_on='user_id', right_index=True, how = 'right')
+
+# drop columns that are not of interest
+cols_to_drop = ['notification_date', 'autosaved', 'notification_fixed',
+                'created_at_x', 'updated_at', 'user_id_reference', 'user_agent',
+                'soundlevel']
+
+# drop specified columns
+merged_df = merged_df.drop(columns=cols_to_drop)
+
+# set answer_id as index
+merged_df.set_index('id', drop = True, inplace = True)
+
+# add country information
+merged_df = pd.merge(merged_df, users_meta[['user_id', 'country']], 
+                     on = 'user_id', how = 'left')
+
+# rename columns, fup = FollowUp, bl= Baseline
+rename_cols = {'save_date':'fup_answer_from',
+               'created_at_y': 'bl_answer_from'}
+merged_df.rename(columns = rename_cols, inplace= True)
+
+# check for matches in tyt plattform from survey and users
+# Spalte 'O' muss ein ja drinstehen
+survey_filtered = survey[survey.iloc[:, -3] == 'ja']
+survey_emails = [mail.lower() for mail in survey_filtered.iloc[:, -2].dropna().unique()
+                 if '@' in mail]
+
+# get user_ids of these mail addresses
+user_ids = users[users['email'].isin(survey_emails)].id.values
+
+# get information of these users from merged
+survey_users = merged_df[merged_df['user_id'].isin(user_ids)]
+# convert user_id to integer
+survey_users = survey_users.astype({'user_id':'int32'})
+
+#%% save data from survey users and merged_df with all users
+survey_users.to_csv('data/02_intermediate/survey_users.csv',
+                    index = False)
+
+merged_df.to_csv('data/02_intermediate/merged_users.csv',
+                    index = True)
